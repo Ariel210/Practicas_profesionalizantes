c286896017264e97d99d4eada74efa126990b1c6
Ariel <33358756+Ariel210@users.noreply.github.com>
2021-06-03 20:17:38 -0300

    Primer ejercicio realizado

--- Ejercicios/FechaNacimientoYEdad.cpp
+++ Ejercicios/FechaNacimientoYEdad.cpp
@@ -1,1 +1,20 @@
-//Realizar un programa que pida la fecha de nacimiento y nos de la edad
+//Realizar un programa que pida la fecha de nacimiento y nos de la edad.
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    int edad;
+    int fechaNac;
+    int anoActual;
+
+    cout<<"Ingrese ano de nacimiento: ";
+    cin >> fechaNac;
+    cout<<"Ingrese ano actual: ";
+    cin >> anoActual;
+    
+    edad = anoActual - fechaNac;
+    cout<<"La edad actual es: "<< edad;
+
+    return 0;
+}
