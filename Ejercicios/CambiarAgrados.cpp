e8d0b07b1b50803082b9fe450d8f6dede97d9076
Ariel <33358756+Ariel210@users.noreply.github.com>
2021-06-06 12:01:59 -0300

    Ejercicio Terminado

--- Ejercicios/CambiarAgrados.cpp
+++ Ejercicios/CambiarAgrados.cpp
@@ -1,1 +1,21 @@
-//Escribe un programa en C++ que lea del teclado un número (real) de grados Fahrenheit y lo convierta a Celsius mostrando el resultado en la pantalla. ºC = 5 / 9 x (ºF ‐ 32)
+//Escribe un programa en C++ que lea del teclado un número (real) de grados Fahrenheit y lo convierta a Celsius mostrando el resultado en la pantalla. ºC = 5 / 9 x (ºF ‐ 32).
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+    float centigrados;
+    float fahrenheit;
+
+    cout<<"Ingrese grados fahrenheit: ";
+    cin>>fahrenheit;
+
+    centigrados = (5.0/9.0) * (fahrenheit-32);
+    cout<<"El resultado final de la temperatura es: "<<centigrados;
+
+    cin.get();
+    cin.ignore();
+    
+    return 0;
+}
