323881c86d200cb8d867d09d21105a0fb2785044
Ariel <33358756+Ariel210@users.noreply.github.com>
2021-06-26 16:56:43 -0300

    Ejercicio 1 de archivos modificado y terminado.

--- Ejercicios.Archivos/Archivos-Ejercicio1.cpp
+++ Ejercicios.Archivos/Archivos-Ejercicio1.cpp
@@ -1,22 +1,22 @@
 /*Crea un programa que vaya leyendo las frases que el usuario teclea y las guarde en un fichero de texto llamado 
 “frasesDeBjarme.txt”. Terminará cuando la frase introducida sea "fin" (esa frase no deberá guardarse en el fichero).*/
-#include<iostream>
-#include<fstream>
-#include<string.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
 using namespace std;
 
 //Prototipo de funcion.
 void creandoArchivo();
 void lectura();
+//void contador();
 
-int main ()
+int main()
 {
     creandoArchivo();
     lectura();
-    
+
     return 0;
 }
-
 
 //Definiendo funcion.
 void creandoArchivo()
@@ -25,42 +25,55 @@ void creandoArchivo()
     string frase;
     string fin = "fin";
     int contador = 0;
-    
-    archivo.open("frasesDeBjarme.txt",ios::out);
+
+    archivo.open("frasesDeBjarme.txt", ios::out);
 
     do
     {
-        cout<<"Ingrese frase: ";
+        cout << "Ingrese frase: ";
         getline(cin, frase);
-    
-        if (frase==fin)
+
+        if (frase == fin)
         {
             break;
         }
         else
         {
-            archivo<<frase<<"\n";
+            archivo << frase << " ";
             contador++;
-        }   
-    
+        }
+
     } while (frase != fin);
 
     archivo.close();
-
 }
 
-void lectura ()
+void lectura()
 {
     ifstream archivo;
     string frase;
+    int cont = 0;
+    string pal;
 
-    archivo.open("frasesDeBjarme.txt",ios::out);
+    archivo.open("frasesDeBjarme.txt", ios::out);
 
     while (!archivo.eof())
     {
-        getline(archivo,frase);
-        cout<<frase<<endl;
+        cout << frase;
+        getline(archivo, frase);
+        // Total de palabras.
+        for(int i=0; frase[i] != '\0'; i++)
+        {
+            if (frase[i] == ' ')
+            {
+                cont++;
+            }
+        }
     }
-    
+    cout << "\nTotal de palabras: "<<cont++;
+
     archivo.close();
+
+    cin.ignore();
+    cin.get();
 }
